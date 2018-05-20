ba969616c1df48abd8376a4054ec22f538dbc412
a1161474617 <32986070+a1161474617@users.noreply.github.com>
2018-05-20 20:36:29 +0800

    new

---
+++ js/my.js
@@ -0,0 +1,26 @@
+﻿$(
+    function () {
+        backTop();
+        backBottom();
+    }
+)
+
+var speed = 300;
+function backTop() {
+    $("#myBtn_1").show();
+    $("#myBtn_1").click(function (e) {
+        $("body,html").animate({
+            scrollTop: 0
+        }, speed);
+    });
+}
+
+function backBottom() {
+    $("#myBtn_2").show();
+    var windowHeight = $("#wrap").height(); //获取页面高度
+    $("#myBtn_2").click(function (e) {
+        $("body").animate({
+            scrollTop: windowHeight
+        }, speed);
+    });
+}
